8f042621703f89b9427d9b03ecbc52a008d9d41c
Hassan <mohammedhassan_shahid@srmuniv.edu.in>
2019-01-08 19:23:19 +0530

    Challenge Completed!

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,11 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Link the views in the layout XML file to the java code.
+        final ImageView ballDisplay = findViewById(R.id.image_eightBall);
+
+        // Stores the ball images in an array.
+        final int[] ballArray = new int[] {
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5
+        };
+
+        // Creating a variable for the button.
+        Button myButton = findViewById(R.id.askButton);
+
+        // Tell the button to listen for clicks.
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // This code gets executed when the button is clicked.
+
+                Random randomNumberGenerator = new Random(); // Creating a variable to store random numbers using Random().
+                int number = randomNumberGenerator.nextInt(5); // Generating random numbers b/w 0-4.
+                int imageResourceId = ballArray[number]; // New variable to store ball image index.
+
+                ballDisplay.setImageResource(imageResourceId); // Assigning the random ball image.
+            }
+        });
     }
 }
